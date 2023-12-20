8e55ebf6343a128ccf74e2eff37e7d6a0b32dc22
necati <necati.arslan2018@gmail.com>
2023-12-20 19:08:21 +0100

    day 2 edit

--- day-2/main.js
+++ day-2/main.js
@@ -2,29 +2,22 @@ const fs = require("fs");
 
 const data = fs.readFileSync("./data.txt", "utf-8").split("\n");
 
-const resultArray = [];
-
 let array = data;
 
 const controlParams = { red: 12, green: 13, blue: 14 };
-const gamePassed = [];
 
-// Kontrol fonksiyonu
+// Control Fuc
 function shouldDelete(element) {
   const GameElements = element.split(":");
-  console.log(GameElements[1]);
   let counts = GameElements[1].split(";").flatMap((item) =>
     item
       .trim()
       .split(",")
       .map((entry) => entry.trim())
   );
-  //console.log(counts);
   for (let j = 0; j < counts.length; j++) {
     const count = parseInt(counts[j]);
     const color = counts[j].split(" ")[1];
-    console.log(color);
-    console.log(count);
 
     if (count > controlParams[color]) {
       return false;
@@ -47,9 +40,7 @@ function sumGame(elements) {
   return sum;
 }
 
-// Elemanları kontrol et ve koşullara uyanları sil
 array = array.filter((item) => shouldDelete(item));
-console.log(array);
 
 const result = sumGame(array);
 console.log(result);
